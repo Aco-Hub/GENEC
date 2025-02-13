ee90391b77933acca83f6a1e1206b53dd75cc609
Sylvia Ekström <sylvia.ekstrom@unige.ch>
2025-02-13 20:59:54 +0100

    Better flow for the creation of the inifile
    The default parameters are now directly accessible by their number.

--- src/main_inifile.f90
+++ src/main_inifile.f90
@@ -9,6 +9,7 @@ program main
 contains
     subroutine input_ini
         implicit none
+        integer:: Input_change, Temp_Var_Int
         character(len=1):: answer
         character(len=4), dimension(4):: sourceid !< source identifier used in output file name
 
@@ -24,78 +25,116 @@ contains
         read(5,*) &
                 GenecStar%zams_velocity
 
-        !!!!! inichemmod bit
-        answer = ''
-
-        do while (answer /= 'y' .and. answer /= 'n' .and. answer /= '1' .and. answer /= '0')
-            write(*,*)'Default settings are:'
-            write(*,*)'         - Asplund-Cunha abundances'
-            write(*,*)'         - scaled solar'
-            write(*,*)'         - Geneva format'
-            write(*,*)'         - small network'
-            write(*,*)'         - structure from pre-calculated model'
-            write(*,*)'Is it ok? (y)es (n)o'
-            read(5,*) answer
-            if (answer /= 'y' .and. answer /= 'n'  .and. answer /= '1' .and. answer /= '0') then
-                write(*,*) 'Please type y or n...'
-            endif
-        enddo
-        if (answer == 'y' .or. answer == 'Y' .or. answer == '1') then
-            GenecStar%idefaut = 1
-        elseif (answer == 'n' .or. answer == 'N' .or. answer == '0') then
-            GenecStar%idefaut = 0
-        endif
+! initialisation to default values
+        GenecStar%source=3
+        GenecStar%alpha=0
+        GenecStar%formatx=2
+        GenecStar%inetwork = 0
+        GenecStar%ipoly = 0
 
         ! source identifier
         sourceid=(/'AG89','GN93','As05','As09'/)
 
-        if (GenecStar%idefaut == 0) then
-        ! choose input file
-            write(*,*) 'Choose source'
-            write(*,*)'Anders & Grevesse 1989: 1 / Grevesse and Noels 1993: 2 / Asplund 2005: 3 / Asplund 2009: 4'
-            read(5,*) &
-                    GenecStar%source
-            write(*,*) sourceid(int(GenecStar%source))
-        ! choose alpha enhanced or solar scaled composition
-            write(*,*)'scaled solar abundances (0) or alpha-enhanced (1)?'
-            read(5,*) &
-                    GenecStar%alpha
+        Input_change = 99
+        do while (Input_change /= 0)
+          write(*,*) '|------------------------------------------------------|'
+          write(*,*) '| Default parameters you can change:                   |'
+          write(*,*) '|------------------------------------------------------|'
+          write(*,*) '|  1: Abundances (default Asplund+2005)                |'
+          write(*,*) '|  2: Scaled solar (default) or alpha-enhanced         |'
+          write(*,*) '|  3: Chemical network (default inetwork=0)            |'
+          write(*,*) '|  4: Initial structure (default pre-calculated model) |'
+          write(*,*) '|  5: File format (default Geneva)                     |'
+          write(*,*) '|------------------------------------------------------|'
+          write(*,*) 'Enter the parameter number you want to change (0 to skip or exit):'
+          read(5,*) Input_change
+          select case(Input_change)
+          case (0)
+            write(*,*) 'No (more) changes...'
+          case (1)
+            Temp_Var_Int = 99
+            do while (Temp_Var_Int > 4)
+              write(*,*) 'Choose the abundance mixture:'
+              write(*,*) '------------------------------'
+              write(*,*) ' 1: Anders & Grevesse 1989'
+              write(*,*) ' 2: Grevesse and Noels 1993'
+              write(*,*) ' 3: Asplund 2005 (default)'
+              write(*,*) ' 4: Asplund 2009'
+              write(*,*) '------------------------------'
+              write(*,*) 'Enter your choice:'
+              read(5,*) Temp_Var_Int
+            enddo
+            if (Temp_Var_Int == 0) then
+              GenecStar%source = 3
+            else
+              GenecStar%source = Temp_Var_Int
+            endif
+            write(*,*) 'Abundance mixture set to ',sourceid(int(GenecStar%source))
+          case (2)
+            Temp_Var_Int = 99
+            do while (Temp_Var_Int > 1)
+              write(*,*) 'Choose the alpha content:'
+              write(*,*) '------------------------------'
+              write(*,*) ' 0: solar-scaled (default)'
+              write(*,*) ' 1: alpha-enhanced'
+              write(*,*) '------------------------------'
+              write(*,*) 'Enter your choice:'
+              read(5,*) Temp_Var_Int
+            enddo
+            GenecStar%alpha = Temp_Var_Int
             write(*,*) 'alpha-enhanced:', GenecStar%alpha
-            if(GenecStar%alpha/=0 .and. GenecStar%alpha/=1) then
-                write(*,*) &
-                        GenecStar
-                stop 'wrong choice - program stopped!'
-            endif
-        ! choose output format
-            write(*,*)'choose format! [1=basnet/2=GENEC/3=PPN input format]'
-            read(5,*) &
-                    GenecStar%formatx
-            if(GenecStar%formatx/=1.and.GenecStar%formatx/=2.and.GenecStar%formatx/=3) then
-                stop 'wrong format choice'
-            endif
-            write(*,*) 'choose network size:'
-            write(*,*) '0: default GENEC - 1: 23 network - 2: 48 network - 3: free network'
-            read(5,*) &
-                     GenecStar%inetwork
+          case (3)
+            Temp_Var_Int = 99
+            do while (Temp_Var_Int > 3)
+              write(*,*) 'Choose network size:'
+              write(*,*) '------------------------------'
+              write(*,*) ' 0: default GENEC'
+              write(*,*) ' 1: 23 network'
+              write(*,*) ' 2: 48 network'
+              write(*,*) ' 3: free network'
+              write(*,*) '------------------------------'
+              write(*,*) 'Enter your choice:'
+              read(5,*) Temp_Var_Int
+            enddo
+            GenecStar%inetwork = Temp_Var_Int
             write(*,*) 'inetwork:',GenecStar%inetwork
-        endif  ! idefaut
-
-        !!!!! end inichemmod bit
-        select case (GenecStar%idefaut)
-            case (0)
-                write(*,*)'Do you want to compute a polytropic structure or use ',&
-                        'a pre-computed structure? Polytrope:1 - structure:0'
-                read(5,*) &
-                        GenecStar%ipoly
-            case (1)
-                GenecStar%ipoly = 0
-        end select
-
-        if (GenecStar%ipoly == 1) then
-            write(*,*)'Enter the polytropic index (recommended: 2.5):'
-            read(5,*) &
-                    GenecStar%n
-        endif
+          case (4)
+            Temp_Var_Int = 99
+            do while (Temp_Var_Int > 1)
+              write(*,*)'Choose the type of initial structure:'
+              write(*,*) '------------------------------'
+              write(*,*) ' 0: Pre-calculated structure (default)'
+              write(*,*) ' 1: Polytrope'
+              write(*,*) '------------------------------'
+              write(*,*) 'Enter your choice:'
+              read(5,*) Temp_Var_Int
+            enddo
+            GenecStar%ipoly = Temp_Var_Int
+            if (GenecStar%ipoly == 1) then
+              write(*,*) 'Enter the polytropic index (recommended 2.5)'
+              read(5,*) GenecStar%n_poly
+            endif
+          case (5)
+            Temp_Var_Int = 99
+            do while (Temp_Var_Int > 3)
+              write(*,*)'Choose the file format:'
+              write(*,*) '------------------------------'
+              write(*,*) ' 1: basnet'
+              write(*,*) ' 2: GENEC (default)'
+              write(*,*) ' 3: PPN input format'
+              write(*,*) '------------------------------'
+              write(*,*) 'Enter your choice:'
+              read(5,*) Temp_Var_Int
+            enddo
+            if (Temp_Var_Int == 0) then
+              GenecStar%formatx = 2
+            else
+              GenecStar%formatx = Temp_Var_Int
+            endif
+          case default
+            write(*,*) 'wrong number: should be between 0 (exit) and 5'
+          end select
+        enddo
 
     end subroutine input_ini
 end program main
