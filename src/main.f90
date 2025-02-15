1d0061f03e794083ce5c40ffb7c641d938b08886
Maxime Marchand <mm.maxime.marchand@gmail.com>
2025-02-15 15:29:39 +0100

    Updated main.f90
    Check if GIT_COMMIT and COMPILATION_DATE are defined. If not,
    a default value is assigned to them ("NOT DEFINED").

--- src/main.f90
+++ src/main.f90
@@ -1,3 +1,11 @@
+#ifndef GIT_COMMIT
+#define GIT_COMMIT "GIT_COMMIT NOT DEFINED"
+#endif
+
+#ifndef COMPILATION_DATE
+#define COMPILATION_DATE "COMPILATION_DATE NOT DEFINED"
+#endif
+
 program main
 
     use genec
